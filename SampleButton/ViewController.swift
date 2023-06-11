c52e14bd90879f2801a5a7e44073a4bb01786baa
taiga07 <taigatani@me.com>
2023-06-11 10:27:33 +0900

    SampleButton

--- SampleButton/ViewController.swift
+++ SampleButton/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dogButton(_ sender: Any) {
+        label.text = "ワンワン"
+    }
+    
+    @IBAction func catButton(_ sender: Any) {
+        label.text = "ニャーニャ"
+    }
+    
+    @IBAction func frogButton(_ sender: Any) {
+        label.text = "ゲロゲロ"
+    }
+    
+    
 }
 
